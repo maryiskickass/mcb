c3f57fb5772c80a5b9802cb69ed4f703025e5bac
dansalias <danyoungnz@gmail.com>
2021-05-12 18:02:59 +0200

    Add articles and links placeholders

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex flex-col min-h-screen text-xl">
     <header class="text-center px-5 py-24">
       <img class="inline-block w-48 h-48" src="/mcb/images/mary.jpg"/>
       <h1 class="mt-8 text-4xl font-semibold">Hello! I am Mary.</h1>
-      <p class="mt-4 mb-8 text-xl">I am a digital media professional currently based in Barcelona, Spain.</p>
-      <p class="mt-4 mb-8 text-xl">I leverage my 12+ years of online advertising experience to help companies of all sizes maximize their revenue.</p>
-      <p class="mt-4 mb-8 text-xl">I'm passionate about integrity, equality and diversity - and how a focus on purposeful inclusion in the workplace will bring success for both employers and employees.</p>
+      <p class="mt-4 mb-8">I am a digital media professional currently based in Barcelona, Spain.</p>
+      <p class="mt-4 mb-8">I leverage my 12+ years of online advertising experience to help companies of all sizes maximize their revenue.</p>
+      <p class="mt-4 mb-8">I'm passionate about integrity, equality and diversity - and how a focus on purposeful inclusion in the workplace will bring success for both employers and employees.</p>
       <ul>
         <li
           v-for="(link, index) in socialLinks"
@@ -18,8 +18,26 @@
         </li>
       </ul>
     </header>
-    <main class="max-w-3xl mx-auto flex-grow p-8">
+    <hr/>
+    <main class="w-full mx-auto flex-grow p-8 grid grid-flow-row grid-cols-2">
+      <section>
+        <h2 class="text-3xl">Articles</h2>
+        <ul class="ml-4 list-disc">
+          <li v-for="article in articles">{{ article.title }}</li>
+        </ul>
+      </section>
+      <section>
+        <h2 class="text-3xl">Links</h2>
+        <ul class="ml-4 list-disc">
+          <li v-for="link in links">
+            <a class="text-blue-700 underline" :href="link.url">
+              {{ link.title }}
+            </a>
+          </li>
+        </ul>
+      </section>
     </main>
+    <hr/>
     <footer class="py-4 text-center">
       Icons by <a href="https://fontawesome.com" target="_new">Font Awesome</a>.
       Source code on <a href="https://github.com/maryiskickass/mcb" target="_new">GitHub</a>.
@@ -31,6 +49,17 @@
 export default {
   name: 'app',
   data: () => ({
+    articles: [
+      {
+        title: 'My First Article',
+      },
+    ],
+    links: [
+      {
+        title: 'Cool Website',
+        url: 'https://iamdan.me/',
+      },
+    ],
     socialLinks: [
       {
         icon: 'linkedin',
